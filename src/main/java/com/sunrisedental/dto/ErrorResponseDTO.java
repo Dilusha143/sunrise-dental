a04fb39061d56bc279edb9d6653c508dc53b1f18
Dilusha <dlwitharana2003@gmail.com>
2026-08-22 20:14:19 +0530

    add DTO for standardized REST error responses

---
+++ src/main/java/com/sunrisedental/dto/ErrorResponseDTO.java
@@ -0,0 +1,15 @@
+package com.sunrisedental.dto;
+
+public class ErrorResponseDTO {
+    private String error;
+
+    public ErrorResponseDTO() {
+    }
+
+    public ErrorResponseDTO(String error) {
+        this.error = error;
+    }
+
+    public String getError() { return error; }
+    public void setError(String error) { this.error = error; }
+}
